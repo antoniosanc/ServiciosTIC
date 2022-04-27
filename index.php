2341fb1f93466cdfb7d5c1808cce16ebf1390e97
antoniosanc <chucho2450@gmail.com>
2022-04-27 15:07:01 -0500

    Index del proyecto

---
+++ index.php
@@ -0,0 +1,300 @@
+<?php include 'header.php';?>
+
+
+
+	<div class="row">
+		<div class="col-md-12" style="padding:0px;">
+				<div class="carousel slide" id="carousel-440287" data-ride="carousel">
+					<!--ol class="carousel-indicators"><li class="active" data-slide-to="0" data-target="#carousel-440287"> </li></ol-->
+					<div class="carousel-inner">
+						<div class="item active " style="background:url(fondos/fondo.jpg) no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; min-height:700px;">
+							<div class="carousel-caption"><h4>Servicios en TIC</h4><p>Hacemos de su negocio un negocio exitoso</p>
+								<p><a href="index.php?q=servicios" class="btn btn-massl">Más Información</a> <a href="index.php?q=contacto" class="btn btn-massl">Contáctanos</a></p>
+							</div>
+							<div class="carousel-caption"><h4>Servicios en TIC</h4><p>Hacemos de su negocio un negocio exitoso</p>
+								<p><a href="index.php?q=servicios" class="btn btn-massl">Más Información</a> <a href="index.php?q=contacto" class="btn btn-massl">Contáctanos</a></p>
+							</div>
+						</div>
+					</div> 
+					<a class="left carousel-control" href="#carousel-440287" data-slide="prev"><img src="./imgusr/sl-izq.png" class="img-responsive" border="0" alt="Izquierda"></a> 
+					<a class="right carousel-control" href="#carousel-440287" data-slide="next"><img src="./imgusr/sl-der.png" class="img-responsive" border="0" alt="Izquierda"></a>
+				</div>
+			</div>
+	</div>
+
+
+	<!-- Bienvenida  -->	
+	<div class="hm-bienvenida" id="no-more-tables">
+		<div class="container">
+			<div class="row">
+				<table width="100%" cellspacing="1" cellpadding="1" border="0" align="center">
+				    <tbody>
+				        <tr>
+				            <td class="text-bienvenida" width="45%">
+				            <h2>BIENVENIDOS</h2>
+				            <p style="text-align: justify;">Somos una empresa dedicada a la venta de <strong>equipo de c&oacute;mputo</strong>, telefon&iacute;a y <strong>telecomunicaciones</strong>, proporcionamos servicios de <strong>soporte t&eacute;cnico</strong>, servicio de instalaci&oacute;n e implementaci&oacute;n de cableado estructurado y fibra &oacute;ptica, instalaci&oacute;n y configuraci&oacute;n de equipo de telecomunicaciones, somos <strong>Microsoft Partner</strong> <strong>Network </strong>por lo que podemos vender todo tipo de licenciamiento Microsoft, somos proveedores de servicios de la nube con <strong>On-Cloud</strong> y <strong>aws</strong> (amazon web services), as&iacute; como de Azure.</p>
+				            <p style="text-align: center;"><a href="index.php?q=servicios" class="btn btn-ver">Ver todos los servicios </a></p>
+				            </td>
+				            <td class="img-bienvenida" width="55%">
+				            <p style="text-align: center;"><img src="userfiles/servicios_tic20/image/inicio/img-bienvenida.jpg" alt="Bienvenido" class="img-responsive" border="0" /></p>
+				            </td>
+				        </tr>
+				    </tbody>
+				</table>
+				<p>&nbsp;</p>
+			</div>
+		</div>
+	</div><br>
+
+<div class="hm-servicios">
+	<div class="container">
+		<div class="row">
+			<div class="row" >
+						<div class="col-sm-4 col-md-4 cards br_col" >
+							<p style="text-align:center;">
+								<a href="index.php?q=soporte_tecnico&n=servicios">
+									<img class="img-responsive" src="userfiles/servicios_tic20/image/imgsoporte-e9f.jpg" border="0" alt="Soporte técnico"/>
+								</a>
+							</p>
+							<h3 style="text-align:center;">Soporte técnico</h3>
+							<p style="text-align:justify;">Soporte Técnico especializado a equipo de cómputo y servidores, Redes Alámbricas...</p>
+							<p style="text-align:center;">
+								<a href="index.php?q=soporte_tecnico&n=servicios" class="btn btn-conoce">CONOCE M&Aacute;S </a>
+							</p>
+						</div>
+						<div class="col-sm-4 col-md-4 cards br_col1" >
+							<p style="text-align:center;">
+								<a href="index.php?q=seguridad_informatica_y_cctv&n=servicios">
+									<img class="img-responsive" src="userfiles/servicios_tic20/image/imgseguridad_informatica-9b7.jpg" border="0" alt="Seguridad Informática y CCTV"/>
+								</a>
+							</p>
+							<h3 style="text-align:center;">Seguridad Informática y CCTV</h3>
+							<p style="text-align:justify;">Te asesoramos en la mejor solución de seguridad de tu infraestructura en donde se...</p>
+							<p style="text-align:center;">
+								<a href="index.php?q=seguridad_informatica_y_cctv&n=servicios" class="btn btn-conoce">CONOCE M&Aacute;S </a>
+							</p>
+						</div>
+						<div class="col-sm-4 col-md-4 cards br_col2" >
+							<p style="text-align:center;">
+								<a href="index.php?q=hosting&n=servicios">
+									<img class="img-responsive" src="userfiles/servicios_tic20/image/imghosting-9dd.jpg" border="0" alt="Hosting"/>
+								</a>
+							</p>
+							<h3 style="text-align:center;">Hosting</h3>
+							<p style="text-align:justify;">Creación y Administración de Dominios (Hosting) Servicios TIC </p>
+							<p style="text-align:center;">
+								<a href="index.php?q=hosting&n=servicios" class="btn btn-conoce">CONOCE M&Aacute;S </a>
+							</p>
+						</div></div>
+							<div class="row " >
+						<div class="col-sm-4 col-md-4 cards br_col0" >
+							<p style="text-align:center;">
+								<a href="index.php?q=comunicaciones_unificadas&n=servicios">
+									<img class="img-responsive" src="userfiles/servicios_tic20/image/imgcomunicaciones-6c9.jpg" border="0" alt="Comunicaciones Unificadas"/>
+								</a>
+							</p>
+							<h3 style="text-align:center;">Comunicaciones Unificadas</h3>
+							<p style="text-align:justify;">Las Comunicaciones Unificadas hacen que las personas se comuniquen, conecten y colaboren...</p>
+							<p style="text-align:center;">
+								<a href="index.php?q=comunicaciones_unificadas&n=servicios" class="btn btn-conoce">CONOCE M&Aacute;S </a>
+							</p>
+						</div>
+						<div class="col-sm-4 col-md-4 cards br_col1" >
+							<p style="text-align:center;">
+								<a href="index.php?q=telecomunicaciones&n=servicios">
+									<img class="img-responsive" src="userfiles/servicios_tic20/image/imgtelecomunicaciones-514.jpg" border="0" alt="Telecomunicaciones"/>
+								</a>
+							</p>
+							<h3 style="text-align:center;">Telecomunicaciones</h3>
+							<p style="text-align:justify;">Se realiza la instalación de equipos dentro del site del cliente de acuerdo a su...</p>
+							<p style="text-align:center;">
+								<a href="index.php?q=telecomunicaciones&n=servicios" class="btn btn-conoce">CONOCE M&Aacute;S </a>
+							</p>
+						</div>
+						<div class="col-sm-4 col-md-4 cards br_col2" >
+							<p style="text-align:center;">
+								<a href="index.php?q=virtualizacion&n=servicios">
+									<img class="img-responsive" src="userfiles/servicios_tic20/image/imgvirtualizacion-03c.jpg" border="0" alt="Virtualización"/>
+								</a>
+							</p>
+							<h3 style="text-align:center;">Virtualización</h3>
+							<p style="text-align:justify;">La virtualización es el proceso de crear una representación basada en software...</p>
+							<p style="text-align:center;">
+								<a href="index.php?q=virtualizacion&n=servicios" class="btn btn-conoce">CONOCE M&Aacute;S </a>
+							</p>
+						</div></div>
+							<div class="row " ></div>			<p align="center"><a href="index.php?q=servicios" class="btn btn-servicios">Ver todos los servicios <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+		</div>
+	</div>
+</div>
+<div class="hm-tu_mejor_opcion">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<h2>Somos tu mejor opci&oacute;n</h2>
+				<div class="panel-group" id="panel-392526">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<a id="panel1" class="panel-title collapsed icono-mas" data-toggle="collapse" data-parent="#panel-392526" href="#bloque1"> 1 a&ntilde;o de garant&iacute;a </a>
+						</div> 
+						<div id="bloque1" class="panel-collapse collapse">
+							<div class="panel-body">
+								<ul>
+									<li>Todos nuestros equipos cuentan m&iacute;nimamente con 1 a&ntilde;o de garant&iacute;a</li>
+									<li>Dependiendo del fabricante hasta 3 a&ntilde;os de garant&iacute;a</li>
+									<li>Nosotros nos hacemos responsables de nuestros equipos en garant&iacute;a</li>
+									<li>En caso de alg&uacute;n problema nuestro cliente nos llama y nosotros realizamos todo el proceso de garant&iacute;a ante el fabricante</li>
+								</ul>
+								<p style="text-align: right;"><a class="btn btn-panel-servicios" href="index.php?q=servicios">Ver todos los servicios <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<a id="panel2" class="panel-title collapsed icono-mas" data-toggle="collapse" data-parent="#panel-392526" href="#bloque2">Garant&iacute;a extendida</a>
+						</div> 
+						<div id="bloque2" class="panel-collapse collapse">
+							<div class="panel-body">
+								<ul>
+									<li>Con algunos fabricantes podemos realizar extensi&oacute;n de garant&iacute;as, con un peque&ntilde;o importe</li>
+								</ul>
+								<p style="text-align: right;"><a class="btn btn-panel-servicios" href="index.php?q=servicios">Ver todos los servicios <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<a id="panel3" class="panel-title collapsed icono-mas" data-toggle="collapse" data-parent="#panel-392526" href="#bloque3">Tecnolog&iacute;a de punta</a>
+						</div> 
+						<div id="bloque3" class="panel-collapse collapse">
+							<div class="panel-body">
+								<ul>
+									<li>En todos nuestros productos manejamos la &uacute;ltima tecnolog&iacute;a de punta</li>
+									<li>En software manejamos las &uacute;ltimas versiones disponibles en el mercado</li>
+								</ul>
+								<p style="text-align: right;"><a class="btn btn-panel-servicios" href="index.php?q=servicios">Ver todos los servicios <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<a id="panel4" class="panel-title collapsed icono-mas" data-toggle="collapse" data-parent="#panel-392526" href="#bloque4">Entrega a domicilio</a>
+						</div> 
+						<div id="bloque4" class="panel-collapse collapse">
+							<div class="panel-body">
+								<ul>
+									<li>Hacemos entregas a domicilio siempre y cuando las instalaciones del cliente est&eacute;n dentro del &aacute;rea metropolitana</li>
+									<li>Aplican restricciones</li>
+								</ul>
+								<p style="text-align: right;"><a class="btn btn-panel-servicios" href="index.php?q=servicios">Ver todos los servicios <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<a id="panel5" class="panel-title collapsed icono-mas" data-toggle="collapse" data-parent="#panel-392526" href="#bloque5">Soporte t&eacute;cnico</a>
+						</div> 
+						<div id="bloque5" class="panel-collapse collapse">
+							<div class="panel-body">
+								<ul>
+									<li>Soporte t&eacute;cnico especializado en sitio o v&iacute;a remota</li>
+									<li>Aplican restricciones</li>
+									<li>P&oacute;lizas de soporte t&eacute;cnico preventivo y correctivo</li>
+								</ul>
+								<p style="text-align: right;"><a class="btn btn-panel-servicios" href="index.php?q=servicios">Ver todos los servicios <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<a id="panel6" class="panel-title collapsed icono-mas" data-toggle="collapse" data-parent="#panel-392526" href="#bloque6">Software original</a>
+						</div> 
+						<div id="bloque6" class="panel-collapse collapse">
+							<div class="panel-body">
+								<ul>
+									<li>Somos distribuidores de una gran variedad de software</li>
+									<li>Sistemas operativos</li>
+									<li>Desarrollo</li>
+									<li>Seguridad</li>
+								</ul>
+								<p style="text-align: right;"><a class="btn btn-panel-servicios" href="index.php?q=servicios">Ver todos los servicios <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				
+			</div>
+		</div>
+	</div>
+</div>
+<div class="hm-azul">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 text-center">
+				<p><img src="./imgusr/img-like.png" class="img-responsive" border="0" alt="Clientes satisfechos"></p>
+				<p>Todos nuestros <br>clientes satisfechos</p>
+			</div>
+			<div class="col-md-6 text-center">
+				<p><img src="./imgusr/img-experiencia.png" class="img-responsive" border="0" alt="10 A&ntilde;os de experiencia"></p>
+				<p>Cumplimos m&aacute;s de 12 a&ntilde;os <br>de experiencia en el giro de las TIC'S</p>
+			</div>
+			<div class="col-md-3 text-center">
+				<p><img src="./imgusr/img-productos.png" class="img-responsive" border="0" alt="productos"></p>
+				<p>M&aacute;s de 500 <br>productos diferentes</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="hm-marcas" id="no-more-tables">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 clientes">
+				
+				<div class="jcarousel-wrapper">
+					<div class="jcarousel carrusel_conv">
+						<ul class="text-center"><li>
+										<a href="index.php?q=marcas"><img class="img-responsive" src="userfiles/servicios_tic20/image/marcas/sl_tripp_litejpg-e380.jpg" alt="tripp-lite" /></a>
+									</li><li>
+										<a href="index.php?q=marcas"><img class="img-responsive" src="userfiles/servicios_tic20/image/marcas/sl_img_hpjpg-3620.jpg" alt="HP logo" /></a>
+									</li><li>
+										<a href="index.php?q=marcas"><img class="img-responsive" src="userfiles/servicios_tic20/image/marcas/sl_img_sbe_techjpg-c37c.jpg" alt="SBE Tech" /></a>
+									</li><li>
+										<a href="index.php?q=marcas"><img class="img-responsive" src="userfiles/servicios_tic20/image/marcas/sl_sl_mirosoftjpg-ac0d.jpg" alt="Microsoft" /></a>
+									</li><li>
+										<a href="index.php?q=marcas"><img class="img-responsive" src="userfiles/servicios_tic20/image/marcas/sl_sl_aspeljpg-6208.jpg" alt="Aspel" /></a>
+									</li><li>
+										<a href="index.php?q=marcas"><img class="img-responsive" src="userfiles/servicios_tic20/image/marcas/sl_slawsjpg-813d.jpg" alt="AWS LOGO" /></a>
+									</li><li>
+										<a href="index.php?q=marcas"><img class="img-responsive" src="userfiles/servicios_tic20/image/marcas/sl_slfortinetjpg-0811.jpg" alt="FORTINET" /></a>
+									</li><li>
+										<a href="index.php?q=marcas"><img class="img-responsive" src="userfiles/servicios_tic20/image/marcas/sl_slpanduitjpg-e920.jpg" alt="PANDUIT" /></a>
+									</li><li>
+										<a href="index.php?q=marcas"><img class="img-responsive" src="userfiles/servicios_tic20/image/marcas/sl_slthorsmanjpg-1537.jpg" alt="THORSMAN" /></a>
+									</li><li>
+										<a href="index.php?q=marcas"><img class="img-responsive" src="userfiles/servicios_tic20/image/marcas/sl_slviavijpg-8124.jpg" alt="VIAVI SOLUTIONS" /></a>
+									</li></ul>			
+					</div>
+					<a href="#" class="jcarousel-control-prev carousel-control2"><img src="./imgusr/sl-izq.png" class="img-responsive" border="0" alt="<-" /></a>
+					<a href="#" class="jcarousel-control-next carousel-control2"><img src="./imgusr/sl-der.png" class="img-responsive" border="0" alt="->" /></a>
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>
+<?php include 'formulario.php' ?>
+<script>
+  window.watsonAssistantChatOptions = {
+      integrationID: "80d51e22-19a4-49d6-8ee4-c2c3f55d494c", // The ID of this integration.
+      region: "us-south", // The region your integration is hosted in.
+      serviceInstanceID: "80825f81-e5d9-4238-8fbc-dda30302d738", // The ID of your service instance.
+      onLoad: function(instance) { instance.render(); }
+    };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js"
+    document.head.appendChild(t);
+  });
+</script>
+<?php include 'footer.php' ?>
