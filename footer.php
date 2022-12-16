80f56f546ea43cc4913d3be12a35e99190b0fd8a
erika <erika.rojano.r@hotmail.com>
2022-12-15 18:43:19 -0600

    ACT1512

--- footer.php
+++ footer.php
@@ -3,11 +3,17 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-4 align-left">
-				<center><h3>Ubicación</h3></center>
-				<p>Chiclayo 639-Oficina 3, Lindavista Sur, Gustavo A. Madero, 07300 Ciudad de México, CDMX</p>
-				<center><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3761.241796902573!2d-99.13410048578068!3d19.48823024395088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1feaf1f19b185%3A0xd1e5d29d3850a4b1!2sServicios%20en%20Tic%20SA%20de%20CV!5e0!3m2!1ses-419!2smx!4v1651267189501!5m2!1ses-419!2smx" width="auto" height="auto" style="border:0;"  loading="lazy" referrerpolicy="no-referrer-when-downgrade" align="center"></iframe></center>
+				<center><h3>Oficinas CDMX</h3></center>
+				<p>Ing. Basiliso Romo Anguiano No. 165, Oficina 3, Col. Industrial, Alcaldía Gustavo A Madero, CP 07800, CDMX</p>
+				<center><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3761.6034889521875!2d-99.13208798255614!3d19.472655099999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f90e919c0573%3A0xfd1ca41fe1b41aeb!2sAv.%20Ing.%20Basiliso%20Romo%20Anguiano%20165%2C%20Industrial%2C%20Gustavo%20A.%20Madero%2C%2007800%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses!2smx!4v1671150476867!5m2!1ses!2smx" width="auto" height="auto" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade" align="center"></iframe></center>
+
 			</div>
 			<div class="col-sm-4 align-left">
+				<center><h3>Oficinas Guadalajara</h3></center>
+				<p>Av. Américas 1254, piso 10, Col. Country Club, Guadalajara, Jal. Torre 500, Best Corporate Offices.</p>
+				<center><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3732.309712706419!2d-103.37566668576046!3d20.697644304376816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428ae3a63f43241%3A0x7a084becd322e9f5!2sAv.%20de%20las%20Am%C3%A9ricas%201254%2C%20Country%20Club%2C%2044610%20Guadalajara%2C%20Jal.!5e0!3m2!1ses!2smx!4v1671151098085!5m2!1ses!2smx" width="auto" height="auto" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" align="center"></iframe></center>
+			</div>
+			<!--<div class="col-sm-4 align-left">
 				<center><h3>Servicios</h3></center>
 				<ul>
 					<li><a href="servicios/soporte_tecnico.php">Soporte t&eacute;cnico</a></li>
@@ -17,11 +23,12 @@
 					<li><a href="servicios/virtualizacion.php">Virtualizaci&oacute;n</a></li>
 					<li><a href="servicios/telecomunicaciones.php">Telecomunicaciones</a></li>
 				</ul>
-			</div>
+			</div> -->
 			<div class="col-sm-4 align-left">
 				<center><h3>Contacto</h3></center>
 				<p><a href="tel:+52-55-9130-1375"><img src="fondos/iconos/llamada.png" alt="Tel&eacute;fono" border="0"> (55) 6732-4800 / (55) 9130-1375</a></p>
 				<p><a href="mailto:soporte@serviciostic.com.mx"><img src="fondos/iconos/correo-electronico.png" alt="Correo" border="0">soporte@serviciostic.com.mx</a></p>
+				<p><a href="mailto:soportegdl@serviciostic.com.mx"><img src="fondos/iconos/correo-electronico.png" alt="Correo" border="0">soportegdl@serviciostic.com.mx</a></p>
 				<p><a href="mailto:ventas@serviciostic.com.mx"><img src="fondos/iconos/correo-electronico.png" alt="Correo" border="0">ventas@serviciostic.com.mx</a></p>
 
 				<p><a href="tel:+52-55-1320-5021"><img src="fondos/iconos/telefono-inteligente.png" alt="Celular" border="0"> (55) 1320 - 5021</a></p>
